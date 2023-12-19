7d8d3b97b4c16c802c9c22db662a27647a84add7
OmkarLande <omkarrlande.534@gmail.com>
2023-12-19 22:41:17 +0530

    implementing dark mode and light mode

--- App.tsx
+++ App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, useColorScheme, Switch } from 'react-native';
 
-import {View, Text, SafeAreaView, Button, Alert} from 'react-native';
+export default function Hello() {
+  const colorScheme = useColorScheme();
+  const [isDarkMode, setIsDarkMode] = useState(colorScheme === 'dark');
 
-const App = () => {
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  useEffect(() => {
+    setIsDarkMode(colorScheme === 'dark');
+  }, [colorScheme]);
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello Omkar Here !!</Text>
-        <Text>Hello Omkar Here !!</Text>
-        <Text>Hello Omkar Here !!</Text>
-        <Text>Hello Omkar Here !!</Text>
-        <Text>Hello Omkar Here !!</Text>
-        <Text>Hello Omkar Here !!</Text>
-        <Button
-          title="Press Me"
-          onPress={() => Alert.alert('Omkar The Mobile Developer!!')}
-        />
+    <View style={[styles.container, isDarkMode ? styles.darkBackground : styles.lightBackground]}>
+      <Text style={[styles.text, isDarkMode ? styles.darkText : styles.lightText]}>Hello Omkar!</Text>
+      <View style={styles.toggleContainer}>
+        <Text style={[styles.text, isDarkMode ? styles.darkText : styles.lightText]}>Dark Mode</Text>
+        <Switch value={isDarkMode} onValueChange={toggleDarkMode} />
+        <Text style={[styles.text, isDarkMode ? styles.darkText : styles.lightText]}>Light Mode</Text>
       </View>
-    </SafeAreaView>
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightBackground: {
+    backgroundColor: '#fff',
+  },
+  darkBackground: {
+    backgroundColor: '#1e1e1e',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#1e1e1e', 
+  },
+  lightText: {
+    color: '#1e1e1e',
+  },
+  darkText: {
+    color: '#fff',
+  },
+  toggleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 20,
+    width: '80%',
+  },
+});
